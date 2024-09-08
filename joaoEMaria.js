0c6dedb11ad8681dbf271b08d4821c38e82690c2
VMacuco <145716210+VMacuco@users.noreply.github.com>
2024-09-08 14:49:28 -0300

    atribuido valores a joao e maria e criadasas funções get no arquivo joaoEMaria.js / impressão dos valores em main.js

--- joaoEMaria.js
+++ joaoEMaria.js
@@ -1,1 +1,15 @@
-console.log('modulo João e Maria');
+//joaoEMaria.js
+let joao = 1;
+let maria = 2;
+
+function getMaria ()
+{
+    return maria;
+}
+
+function getJoao ()
+{
+    return joao;
+}
+
+export{getJoao, getMaria}
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import  {getJoao, getMaria} from "./joaoEMaria.js";
+
+//main.js
+
 console.log('modulo main');
+
+console.log('João tem ', getJoao(), ' Maçãs!');
+console.log('Maria tem ', getMaria(), ' Maçãs!');
